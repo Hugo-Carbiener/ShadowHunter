efeb247727786311af7d87350ef26aac5ac7473f
Hugo's Laptop <hugocarbiener@gmail.com>
2021-07-12 17:42:10 +0200

    Add 7 cards

---
+++ shadowHunter/src/card/RevolverDesTenebres.java
@@ -0,0 +1,11 @@
+package card;
+
+public class RevolverDesTenebres {
+	
+	protected CardType Equipement;
+	protected CardAlignement DARKNESS;
+	
+	public void effect() {
+		//Vous pouvez attaquer les personnages dans les zones différentes de la votre uniquement
+	}
+}
---
+++ shadowHunter/src/card/TronconneuseDuMal.java
@@ -0,0 +1,11 @@
+package card;
+
+public class TronconneuseDuMal {
+	
+	protected CardType Equipement;
+	protected CardAlignement DARKNESS;
+	
+	public void effect() {
+		//+1 blessure si on en inflige 
+	}
+}

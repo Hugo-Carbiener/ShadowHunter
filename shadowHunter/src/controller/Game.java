e1bf332824913d8b8df6a8675c219264691f7d96
ElMitsuki <tommyramos9876@gmail.com>
2021-07-13 15:27:34 +0200

    ajout du package controller et de la classe Game

---
+++ shadowHunter/src/controller/Game.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class Game {
+	
+	private int nbPlayer;
+	private String actualPLayer;
+	
+	public void init() {
+		
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void loop() {
+		
+	}
+	
+	public void end() {
+		
+	}
+
+}

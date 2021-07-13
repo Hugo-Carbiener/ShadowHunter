272e84e7395be0a4c465d0bcd01fe348db41012e
ElMitsuki <tommyramos9876@gmail.com>
2021-07-13 16:06:43 +0200

    ajout de la pioche dans Game

--- shadowHunter/src/controller/Game.java
+++ shadowHunter/src/controller/Game.java
@@ -1,9 +1,16 @@
 package controller;
 
+import java.util.List;
+
+import card.Card;
+
 public class Game {
 	
 	private int nbPlayer;
 	private String actualPLayer;
+	private List<Card> drawLight;
+	private List<Card> drawDarkness;
+	private List<Card> drawVision;
 	
 	public void init() {
 		

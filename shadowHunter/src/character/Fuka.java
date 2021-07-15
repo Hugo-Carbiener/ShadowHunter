3fd989183c756b4888755323e9aa29eb69e7a498
Hugo's Laptop <hugocarbiener@gmail.com>
2021-07-15 15:13:33 +0200

    Add Fuka's win condition

--- shadowHunter/src/character/Fuka.java
+++ shadowHunter/src/character/Fuka.java
@@ -1,6 +1,7 @@
 package character;
 
 import controller.Game;
+import player.Player;
 
 public class Fuka extends Character {
 
@@ -15,10 +16,23 @@ public class Fuka extends Character {
 		//unique
 	}
 
-	@Override
 	public boolean victoryCondition(Game game) {
-		// Mort shadow
-		return false;
+		
+		boolean shadowsLeft = false;
+		//Check if some shadows are still alive
+		for(Player player : game.getPlayer()) {
+			if (player.getCharacter().role == Role.SHADOW) {
+				shadowsLeft = true; //There are still shadows left, hunter hasn't won yet
+			}
+		}
+		
+		//If there is no shadows left, hunter has won
+		if (! shadowsLeft) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
 }

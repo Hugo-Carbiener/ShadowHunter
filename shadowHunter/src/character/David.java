02dfee47c30952b0f8514ba7bb33f9b739d0a5f3
Hugo's Laptop <hugocarbiener@gmail.com>
2021-07-15 16:02:28 +0200

    Add David's win condition

--- shadowHunter/src/character/David.java
+++ shadowHunter/src/character/David.java
@@ -1,5 +1,12 @@
 package character;
 
+import java.util.List;
+
+import card.Amulette;
+import card.Card;
+import card.CrucifixEnArgent;
+import card.LanceDeLonginus;
+import card.TogeSainte;
 import controller.Game;
 
 public class David extends Character {
@@ -16,7 +23,20 @@ public class David extends Character {
 
 	public boolean victoryCondition(Game game) {
 		// au moins 3 parmi crucifix en argent,amulette,lance de longinus, toge sainte
-		return false;
+		List<Card> inventory = player.getEquipment();
+		int counter = 0;
+		
+		for (Card equipement : inventory) {
+			if (equipement instanceof CrucifixEnArgent || equipement instanceof Amulette || equipement instanceof LanceDeLonginus || equipement instanceof TogeSainte) {
+				counter++;
+			}
+		}
+		
+		if (counter >= 3) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
